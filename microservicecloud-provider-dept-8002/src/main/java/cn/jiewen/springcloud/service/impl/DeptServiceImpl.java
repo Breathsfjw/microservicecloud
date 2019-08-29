7377f32290d9d370f73263e61161e8757081a150
Breathsfjw <13155872663@163.com>
2019-08-29 16:53:20 +0800

    commit first

---
+++ microservicecloud-provider-dept-8002/src/main/java/cn/jiewen/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,35 @@
+package cn.jiewen.springcloud.service.impl;
+
+import cn.jiewen.springcloud.service.DeptService;
+import cn.jiewen.springcloud.dao.DeptDao;
+import cn.jiewen.springcloud.entities.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService
+{
+    @Autowired
+    private DeptDao deptDao;
+
+    @Override
+    public boolean add(Dept dept)
+    {
+        return deptDao.addDept(dept);
+    }
+
+    @Override
+    public Dept get(Long id)
+    {
+        return deptDao.findById(id);
+    }
+
+    @Override
+    public List<Dept> list()
+    {
+        return deptDao.findAll();
+    }
+
+}
